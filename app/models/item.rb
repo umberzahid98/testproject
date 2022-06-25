6fab513fe157366f55ba1750b6e949efb7906774
Dev <dev@Devs-MacBook-Pro.local>
2022-06-25 17:57:44 +0500

    active storage image
    * active storage image used * image custom validatoin

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,20 @@
 class Item < ApplicationRecord
+  has_one_attached :image
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true
   validates :price,  :numericality => { :only_float => true, :greater_than => 0 }
+  validate :image_type
+
+
+  private
+  def image_type
+    if (image.attached?)== false
+      errors.add(:image, "is missing")
+    end
+
+  end
+
+
 
 
 end
