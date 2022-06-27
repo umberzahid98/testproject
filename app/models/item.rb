fb85c103a195a76f8b868b5f95f85ccb850d3f10
Dev <dev@Devs-MacBook-Pro.local>
2022-06-27 18:02:54 +0500

    validations of price

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   has_one_attached :image
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true
-  validates :price, numericality: { only_float: true, greater_than: 0 }
+  validates :price, presence: true, numericality: { only_float: true, greater_than: 0 }
   validate :image_type
 
   private
