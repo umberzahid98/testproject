dd3d0d7640bcf1a2e3bd955a08a937fbf9a54373
Dev <dev@Devs-MacBook-Pro.local>
2022-07-05 17:23:23 +0500

    in next changing ids of divs

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -55,20 +55,37 @@ class ItemsController < ApplicationController
       end
       format.js
     end
+
   end
 
   # PATCH/PUT /items/1 or /items/1.json
   def update
+
+    if params{item[:status]}
+
+      if current_user
+        if current_user.admin?
+          @items = Item.all
+        else
+          @items=Item.permited_items
+        end
+      end
+      unless current_user
+          @items = Item.permited_items
+      end
+    end
     respond_to do |format|
       if @item.update(item_params)
         format.html { redirect_to item_url(@item), notice: 'Item was successfully updated.' }
         format.json { render :show, status: :ok, location: @item }
+
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @item.errors, status: :unprocessable_entity }
       end
       format.js
     end
+
   end
 
   # DELETE /items/1 or /items/1.json
@@ -91,6 +108,6 @@ class ItemsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def item_params
-    params.require(:item).permit(:title, :description, :price, :image, category_ids: [])
+    params.require(:item).permit(:title, :description, :price, :status,:image, category_ids: [])
   end
 end
